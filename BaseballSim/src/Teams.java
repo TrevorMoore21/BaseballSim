5cdbdb06c99021d6e6f130c37667326a7773ac89
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2022-02-17 10:12:22 -0700

    thursday's work

--- BaseballSim/src/Teams.java
+++ BaseballSim/src/Teams.java
@@ -12,8 +12,54 @@ public class Teams
 		static ArrayList<AllStarPlayer> Legends = new ArrayList<AllStarPlayer>();
 		
 		
+		public static void fillRubberDucks()
+			{
+			
+				
+				
+			}
 		
+		public static void fillYardGoats()
+			{
+				
+				
+				
+			}
 		
+		public static void fillBiscuits()
+			{
+				
+				
+				
+			}
+		
+		public static void fillPinkertons()
+			{
+				
+				
+				
+			}
+		
+		public static void fillElFarts()
+			{
+				
+				
+				
+			}
+		
+		public static void fillChicharrones()
+			{
+				
+				
+				
+			}
+		
+		public static void fillLegends()
+			{
+				
+				
+				
+			}
 		
 		
 		
